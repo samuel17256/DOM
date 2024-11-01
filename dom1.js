15f7c7949dc93d5559df1167c52e761de64cb37c
samuel17256 <samuelnanbam@gmail.com>
2024-11-01 07:50:51 +0000

    initial commit

---
+++ dom1.js
@@ -0,0 +1,18 @@
+let colors=['yellow','black','red','blue','gray']
+i=0;
+i<colors.length
+function changeColors(){
+    document.body.style.backgroundColor=colors[i];
+    btn.style.color='purple'
+        document.getElementById
+        btn.style.color='purple'
+            document.getElementById("btn").innerHTML=`change to ${colors[i]}`
+            i=(i+1) %colors.length;("btn").innerHTML=`change to ${colors[i]}`
+        i=(i+1) %colors.length;
+btn.style.backgroundColor=colors[i];
+btn.style.color='purple'
+    document.getElementById("btn").innerHTML=`change to ${colors[i]}`
+    i=(i+1) %colors.length;
+}
+let button=document.getElementById("btn");
+button.addEventListener('click',changeColors)
